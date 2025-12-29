3f65abcffaf2ff4d54309dc7ce10ecb60df860ae
Eliot W. Robson <eliot.robson24@gmail.com>
2025-12-29 00:07:11 -0500

    Version number bump

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ copyright = "{year}, Earth Lab Contributors, Leah Wasser, Pete Stenger".format(
 author = "Leah Wasser"
 
 # The short X.Y version
-version = "0.2.0"
+version = "0.2.1"
 # The full version, including alpha/beta/rc tags
 release = version
 
